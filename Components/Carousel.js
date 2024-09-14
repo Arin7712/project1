d8b3b67575c8ff68b1998fb18455495cc76671cf
Arin7712 <aringawande7712@gmail.com>
2024-09-14 12:44:02 +0530

    third

--- Components/Carousel.js
+++ Components/Carousel.js
@@ -4,9 +4,7 @@ import React from 'react'
 import {
     Carousel,
     CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
+    CarouselItem
   } from "@/components/ui/carousel"
 import Image from 'next/image'
 import { useState, useEffect } from "react"
